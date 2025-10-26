cfd4ea47f6fe7a11dcae16c27f15b654e2f2681b
toanlcgift <toanlcgift@gmail.com>
2025-10-26 12:46:17 +0700

    update

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -12,7 +12,6 @@
 	import { get, type Unsubscriber, type Writable } from 'svelte/store';
 	import type { i18n as i18nType } from 'i18next';
 	import { WEBUI_BASE_URL } from '$lib/constants';
-	import JavascriptCodeEditor from '$lib/components/common/JavascriptCodeEditor.svelte';
 
 	import {
 		chatId,
--- src/routes/(app)/c/[id]/+page.svelte
+++ src/routes/(app)/c/[id]/+page.svelte
@@ -2,8 +2,6 @@
 	import { page } from '$app/stores';
 
 	import Chat from '$lib/components/chat/Chat.svelte';
-	import PhonePreview from '$lib/components/chat/PhonePreview.svelte';
-	import JavascriptCodeEditor from '$lib/components/common/JavascriptCodeEditor.svelte';
 </script>
 
 <Chat chatIdProp={$page.params.id} />
